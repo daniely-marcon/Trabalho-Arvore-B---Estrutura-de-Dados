a7c3e52a3ad3e21efc7273d9d442b4c25ae5554d
Daniely Marcon <daniellymaximo13@gmail.com>
2026-05-19 20:15:03 -0400

    Arquivos Base
    Add: Adicionando arquivos base do programa.

---
+++ arvore_b.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int main(){
+	int op;
+
+
+	do{
+		printf("//----- //----- // ARVORE B //----- //----- //\n[1]- Buscar\n[2]- Inserir\n[9]- Finalizar\n------------------------------\nEntre com a sua opcao:");
+		scanf("%d",&op);
+		if(op==1){
+
+		}else if(op==2){
+
+		}
+	}while(op!=9);
+
+
+	return 0;
+}

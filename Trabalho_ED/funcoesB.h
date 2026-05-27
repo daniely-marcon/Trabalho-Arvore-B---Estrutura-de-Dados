4b92fba8525934330f8dcdbbf1d9b6ee6f52dd3d
Giovanna Teodoro <giovanna_teodoro@ufms.br>
2026-05-27 11:35:25 -0400

    Create funcoesB.h

---
+++ Trabalho_ED/funcoesB.h
@@ -0,0 +1,19 @@
+typedef struct arvore_b{
+	int n ; // numero de chaves armazenadas 
+	char **chave; // Chave armazenada no no 
+	struct arvore_b **filhos; // numero de filhos que o no possui 
+	int folha; // indicaçao se é um no folha ou nao 
+
+
+} arvore_b; 
+
+/* function prototypes */
+arvore_b *aloca(int ordem);
+arvore_b *inserir_b(arvore_b *ptr, char nome[], int ordem);
+void inserirB_N(arvore_b *ptr, char nome[], int ordem);
+void divide_no(arvore_b *ptr, int i, int ordem);
+arvore_b *buscaB(char nome[], arvore_b *ptr);
+void imprimir(arvore_b *ptr, int nivel);
+void finalizar(arvore_b *ptr);
+
+
